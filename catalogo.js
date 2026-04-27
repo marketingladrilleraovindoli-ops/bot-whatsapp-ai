9c4d6ce5bcf8eb159f0fcfdcd2e56673850b839f
marketingladrilleraovindoli-ops <marketingladrilleraovindoli@gmail.com>
2026-04-27 07:52:15 -0500

    Export catalogo object with image URLs

--- catalogo.js
+++ catalogo.js
@@ -1,76 +1,121 @@
-const catalogo = {
+export const catalogo = {
+
+  // ==============================
+  // ADOQUINES
+  // ==============================
   adoquin_20x10x3: {
     nombre: "Adoquín 20x10x3",
     rendimiento: 50,
     tonos: ["durazno", "canelo", "matizado"],
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/8Km9tLL.jpg",
+      "https://i.imgur.com/5yY6FZB.jpg"
+    ]
   },
 
   adoquin_20x10x4: {
     nombre: "Adoquín 20x10x4",
     rendimiento: 50,
     tonos: ["durazno", "canelo", "matizado"],
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/4AiXzf8.jpg",
+      "https://i.imgur.com/ZVnXkFZ.jpg"
+    ]
   },
 
   adoquin_20x10x6: {
     nombre: "Adoquín 20x10x6",
     rendimiento: 50,
     tonos: ["durazno", "canelo", "matizado"],
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/jTq2K8W.jpg",
+      "https://i.imgur.com/9l3A3pO.jpg"
+    ]
   },
 
   adoquin_20x10x8: {
     nombre: "Adoquín 20x10x8",
     rendimiento: 50,
     tonos: ["durazno", "canelo", "matizado"],
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/BG0yW9k.jpg",
+      "https://i.imgur.com/2WZtODx.jpg"
+    ]
   },
 
   adoquin_24x12x6: {
     nombre: "Adoquín 24x12x6",
     rendimiento: 34,
     tonos: ["canelo", "matizado"],
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/7rl4FQy.jpg",
+      "https://i.imgur.com/6pK9x7F.jpg"
+    ]
   },
 
   adoquin_ecologico: {
     nombre: "Adoquín ecológico 24x12x6",
     rendimiento: 45,
     tonos: ["matizado"],
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/eY7Qw0X.jpg",
+      "https://i.imgur.com/3W8Z8TQ.jpg"
+    ]
   },
 
   adoquin_corbatin: {
     nombre: "Adoquín corbatín 20x10x6",
     rendimiento: 55,
     tonos: ["durazno", "canelo", "matizado"],
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/n4K9tZp.jpg",
+      "https://i.imgur.com/6M3p7yP.jpg"
+    ]
   },
 
   adoquin_cuarteron: {
     nombre: "Adoquín cuarterón 24x6x6",
     rendimiento: 68,
-    imagenes: []
+    tonos: ["natural"],
+    imagenes: [
+      "https://i.imgur.com/0Z8FQpK.jpg",
+      "https://i.imgur.com/4d6wYgF.jpg"
+    ]
   },
 
+  // ==============================
+  // FACHALLETAS (THINBRICK)
+  // ==============================
   fachaleta_cappuccino: {
     nombre: "Fachaleta Cappuccino",
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/v6L0F7E.jpg",
+      "https://i.imgur.com/JQw4cFv.jpg"
+    ]
   },
 
   fachaleta_nero: {
     nombre: "Fachaleta Nero Piamonte",
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/JpZzK8k.jpg",
+      "https://i.imgur.com/6Wl9QxX.jpg"
+    ]
   },
 
   fachaleta_toscano: {
     nombre: "Fachaleta Toscano / Romano",
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/HTj7pXQ.jpg",
+      "https://i.imgur.com/lY0c0o9.jpg"
+    ]
   },
 
   fachaleta_bianco: {
     nombre: "Fachaleta Bianco Ártico",
-    imagenes: []
+    imagenes: [
+      "https://i.imgur.com/YbKp2QY.jpg",
+      "https://i.imgur.com/Q5uY9eF.jpg"
+    ]
   }
+
 };
